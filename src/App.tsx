1bcaabd20a0c75df68f30f550b3bd3c3f19f40ca
anton pastukhov <antonpastukhov@MacBook-Air-anton.local>
2022-02-15 14:11:49 +0300

    emotion try

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home/Home';
+import Chart from './components/Chart/Chart';
+// import Home from './components/Home/Home';
 
 function App() {
 
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Chart />
     </div>
   );
 }
